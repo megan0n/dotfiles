a6524819662b92d5ff2cda5c9df3d2daa828249e
arekusandu <oriondo.abp@gmail.com>
2021-07-24 02:57:20 -0700

    Doom emacs installation

--- emacs.sh
+++ emacs.sh
@@ -5,7 +5,17 @@ sudo apt install build-essential git
 sudo apt build-dep emacs25
 
 cd emacs
-    ./autogen.sh
-    ./configure
-    make bootstrap
-    sudo make install
+./autogen.sh
+./configure
+make bootstrap
+sudo make install
+
+#doom requires git > 2.23
+sudo apt-get install devscripts dpkg-dev build-essential
+sudo apt-get build-dep git
+echo "deb http://deb.debian.org/debian buster-backports main" > /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get -t buster-backports install git
+~/.emacs.d/bin/doom sync
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
