6474dec23ae18cc500ed252834387b785a4eb72f
arekusandu <oriondo.abp@gmail.com>
2021-07-27 23:41:30 -0700

    Add gtk dark

--- run-config.sh
+++ run-config.sh
@@ -5,9 +5,20 @@
 sudo apt-get update
 sudo apt-get install -y i3 vim feh mpv terminator spacefm ssh sshfs git \
 	python3 python3-pip rsync curl qbittorent \
-	dia gnumeric
+	dia gnumeric flameshot
 
 #list dependencies to file
 tar xzf package.tar.gz
 python setup.py instaell --record file.txt
 xarfs rm -rf < file.txt
+
+#i3-gtk compatibility issue
+exec --no-startup-id /usr/lib/gsd-xsettings
+
+#gtk adwaita-dark
+sudo apt-get install lxappearance gtk-chtheme qt4-qtconfig
+#papirus icon-theme
+wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.icons" sh
+
+#flameshot-i3 keybinding
+echo "bindsym $mod+p flameshot gui -p ~/Pictures" >> ~/.config/i3/config
