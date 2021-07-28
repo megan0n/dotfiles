877b0207ddef28981adb725c2ebe5f44a651cd87
arekusandu <oriondo.abp@gmail.com>
2021-07-27 17:48:13 -0700

    Add doom emacs installation sh

--- emacs.sh
+++ emacs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#emacs27
 git clone --depth 1 --branch emacs-27 https://git.savannah.gnu.org/git/emacs.git
 sudo apt install build-essential git
 sudo apt build-dep emacs25
@@ -9,13 +10,15 @@ cd emacs
 ./configure
 make bootstrap
 sudo make install
+cd
 
-#doom requires git > 2.23
+#git 2.23
 sudo apt-get install devscripts dpkg-dev build-essential
 sudo apt-get build-dep git
 echo "deb http://deb.debian.org/debian buster-backports main" > /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get -t buster-backports install git
-~/.emacs.d/bin/doom sync
+
+#doom
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
--- run-config.sh
+++ run-config.sh
@@ -4,4 +4,10 @@
 #Applications
 sudo apt-get update
 sudo apt-get install -y i3 vim feh mpv terminator spacefm ssh sshfs git \
-	python3 python3-pip rsync curl
+	python3 python3-pip rsync curl qbittorent \
+	dia gnumeric
+
+#list dependencies to file
+tar xzf package.tar.gz
+python setup.py instaell --record file.txt
+xarfs rm -rf < file.txt
