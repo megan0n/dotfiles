7918ea62d574068e89af0e05462fff6270942a5f
ben <oriondo.abp@gmail.com>
2021-08-13 02:31:46 +0800

    Add gnome-i3 config

---
+++ i3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get install i3
+
+#access gnome-control-center from i3
+sudo chmod +x settings 
+sudo cp settings /usr/local/bin
+
+#in /.config/i3/config
+default_border pixel 0
+default_floating_border pixel 0
+
